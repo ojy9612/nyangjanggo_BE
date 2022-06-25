17836067fa4f27b3e3c1a64d4c1c0501409a5d28
chanwoo yang <moomsss@naver.com>
2022-06-25 16:24:55 +0900

    Board 엔티티 구현 #19

--- src/main/java/com/hanghae99_team3/model/board/Board.java
+++ src/main/java/com/hanghae99_team3/model/board/Board.java
@@ -2,6 +2,8 @@ package com.hanghae99_team3.model.board;
 
 
 import com.hanghae99_team3.model.Timestamped;
+import com.sun.istack.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,14 +23,22 @@ public class Board extends Timestamped {
 
     private String content;
 
-    private String img;
+    private String imgLink;
+    private String imgKey;
 
-
-    public Board(Long id, String title, String content, String img) {
-        this.id = id;
+    @Builder
+    public Board(@NotNull String title, @NotNull String content, String imgLink, String imgKey) {
         this.title = title;
         this.content = content;
-        this.img = img;
+        this.imgLink = imgLink;
+        this.imgKey = imgKey;
+    }
+    public void update(BoardRequestDto boardRequestDto){
+        this.title = boardRequestDto.getTitle();
+        this.content = boardRequestDto.getContent();
+        this.imgLink = boardRequestDto.getImgLink();
+        this.imgKey = boardRequestDto.getImgKey();
+
     }
 }
 
