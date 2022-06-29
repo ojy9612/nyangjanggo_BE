aa508b8457ab2c7d33b6d02bc1e3e0c18c4f406c
chanwoo yang <moomsss@naver.com>
2022-06-30 00:20:17 +0900

    board paging 기능 구현 및 LazyInitializationException No Session 관련 오류 수정 # 51

--- src/main/java/com/hanghae99_team3/model/board/BoardRepository.java
+++ src/main/java/com/hanghae99_team3/model/board/BoardRepository.java
@@ -1,5 +1,8 @@
 package com.hanghae99_team3.model.board;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -7,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    @Override
+    Page<Board> findAll(Pageable pageable);
 }
