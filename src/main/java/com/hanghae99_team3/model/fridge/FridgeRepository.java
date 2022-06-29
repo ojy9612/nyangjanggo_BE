4c4e9f4e6a5cac01836df83a8c300a31c2971422
ojy9612 <pvnm@naver.com>
2022-06-29 23:05:53 +0900

    Fridge 기능 구현 #47

---
+++ src/main/java/com/hanghae99_team3/model/fridge/FridgeRepository.java
@@ -0,0 +1,6 @@
+package com.hanghae99_team3.model.fridge;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FridgeRepository extends JpaRepository<Fridge, Long> {
+}
