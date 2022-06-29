8bbddec094215a57eac61c7ac6a5bb181d8ce3fc
Hogyun Kim <ggyuniH0@gmail.com>
2022-06-29 09:10:16 +0900

    리팩터링

--- src/main/java/com/hanghae99_team3/model/board/BoardRepository.java
+++ src/main/java/com/hanghae99_team3/model/board/BoardRepository.java
@@ -1,12 +1,10 @@
 package com.hanghae99_team3.model.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 
 
-//@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
 }
