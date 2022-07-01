62bc389ed384410b3ac0b59f0b18ea2d2ae6ffa2
ojy9612 <pvnm@naver.com>
2022-07-01 12:37:31 +0900

    Resource 기능 추가 #48

--- src/main/java/com/hanghae99_team3/model/resource/ResourceRepository.java
+++ src/main/java/com/hanghae99_team3/model/resource/ResourceRepository.java
@@ -1,6 +1,9 @@
 package com.hanghae99_team3.model.resource;
 
+import com.hanghae99_team3.model.board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
+
+    void deleteAllByBoard(Board board);
 }
