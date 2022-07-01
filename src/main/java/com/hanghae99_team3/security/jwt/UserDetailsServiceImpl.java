35d4b264dccc93dba9aaf3d349084fc4edfa5188
Hogyun Kim <ggyuniH0@gmail.com>
2022-07-01 09:00:07 +0900

    #37 user api 리팩터링 1. username -> nickname 2. PrincipalDetails.java 에서 User 객체 제거

--- src/main/java/com/hanghae99_team3/security/jwt/UserDetailsServiceImpl.java
+++ src/main/java/com/hanghae99_team3/security/jwt/UserDetailsServiceImpl.java
@@ -14,8 +14,8 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
         return new PrincipalDetails(user);
     }
--- src/main/java/com/hanghae99_team3/security/oauth2/PrincipalDetails.java
+++ src/main/java/com/hanghae99_team3/security/oauth2/PrincipalDetails.java
@@ -1,6 +1,8 @@
 package com.hanghae99_team3.security.oauth2;
 
+import com.hanghae99_team3.model.user.domain.AuthProvider;
 import com.hanghae99_team3.model.user.domain.User;
+import com.hanghae99_team3.model.user.domain.UserRole;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,28 +14,70 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
-@Getter
+
 @ToString
 public class PrincipalDetails implements UserDetails, OAuth2User {
 
-    private final User user;
+    private final String nickname;
+    private final String userImg;
+    private final String userDescription;
+    private final String email;
+    private final UserRole role;
+    private final AuthProvider authProvider;
     private OAuth2UserInfo oAuth2UserInfo;
+
 
     //UserDetails: Form 로그인
     public PrincipalDetails(User user) {
-        this.user = user;
+        this.nickname = user.getNickname();
+        this.userImg = user.getUserImg();
+        this.userDescription = user.getUserDescription();
+        this.email = user.getEmail();
+        this.role = user.getRole();
+        this.authProvider = user.getAuthProvider();
     }
 
     //OAuth2User : OAuth2 로그인
     public PrincipalDetails(User user, OAuth2UserInfo oAuth2UserInfo) {
-        this.user = user;
+        this.nickname = user.getNickname();
+        this.userImg = user.getUserImg();
+        this.userDescription = user.getUserDescription();
+        this.email = user.getEmail();
+        this.role = user.getRole();
+        this.authProvider = user.getAuthProvider();
+
         this.oAuth2UserInfo = oAuth2UserInfo;
     }
 
 
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getUserImg() {
+        return userImg;
+    }
+
+    public String getUserDescription() {
+        return userDescription;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public AuthProvider getAuthProvider() {
+        return authProvider;
+    }
+
+    /**
+     * UserDetails Methods
+     * @return
+     */
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole().getAuthority());
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(role.getAuthority());
         Collection<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(simpleGrantedAuthority);
         return authorities;
@@ -41,14 +85,13 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
 
     @Override
     public String getPassword() {
-//        return user.getPassword();
         return null;
     }
 
 
     @Override
     public String getUsername() {
-        return user.getEmail();
+        return email;
     }
 
     @Override
@@ -76,7 +119,6 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
      * OAuth2User Methods
      * @return
      */
-
     @Override
     public Map<String, Object> getAttributes() {
 //        return attributes;
@@ -88,6 +130,5 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
     public String getName() {
         return oAuth2UserInfo.getProviderId();
     }
-
 
 }
