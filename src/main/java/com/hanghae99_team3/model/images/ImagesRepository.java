f0070c3eb448627ab2131631feb4fe84258d7e3f
ojy9612 <pvnm@naver.com>
2022-07-02 22:56:10 +0900

    기타 전체파일

--- src/main/java/com/hanghae99_team3/model/images/ImagesRepository.java
+++ src/main/java/com/hanghae99_team3/model/images/ImagesRepository.java
@@ -4,8 +4,12 @@ import com.hanghae99_team3.model.board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ImagesRepository extends JpaRepository<Images, Long> {
 
     List<Images> findAllByBoard(Board board);
+
+    Optional<Images> findByImageLink(String imageLink);
+
 }
