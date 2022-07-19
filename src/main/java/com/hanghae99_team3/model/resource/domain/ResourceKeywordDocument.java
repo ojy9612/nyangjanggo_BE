16a8fc916864164986a8cd8d46b70dad8af07509
ojy9612 <pvnm@naver.com>
2022-07-19 19:48:53 +0900

    재료검색어 테이블 생성, 재료기반 추천검색어, 검색목록 구현 #108

---
+++ src/main/java/com/hanghae99_team3/model/resource/domain/ResourceKeywordDocument.java
@@ -0,0 +1,38 @@
+package com.hanghae99_team3.model.resource.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.*;
+
+import javax.persistence.Id;
+
+@Getter
+@Document(indexName = "resource_keyword")
+@NoArgsConstructor
+@Mapping(mappingPath = "elastic/resource-mapping.json")
+@Setting(settingPath = "elastic/nori-setting.json")
+public class ResourceKeywordDocument {
+
+    @Id
+    private Long id;
+
+    private String resourceName;
+
+    private Integer cnt;
+
+    public void plusCnt(){
+        this.cnt += 1;
+    }
+
+    public void minusCnt(){
+        if (cnt > 0) this.cnt -= 1;
+    }
+
+    @Builder
+    public ResourceKeywordDocument(Resource resource) {
+        this.id = resource.getId();
+        this.resourceName = resource.getResourceName();
+        this.cnt = 0;
+    }
+}
--- src/main/java/com/hanghae99_team3/model/resource/service/ResourceService.java
+++ src/main/java/com/hanghae99_team3/model/resource/service/ResourceService.java
@@ -1,14 +1,18 @@
 package com.hanghae99_team3.model.resource.service;
 
 import com.hanghae99_team3.model.board.domain.Board;
+import com.hanghae99_team3.model.resource.domain.ResourceKeywordDocument;
 import com.hanghae99_team3.model.resource.repository.ResourceRepository;
 import com.hanghae99_team3.model.resource.domain.Resource;
 import com.hanghae99_team3.model.resource.dto.ResourceRequestDto;
+import com.hanghae99_team3.model.resource.repository.ResourceSearchRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +20,9 @@ import java.util.List;
 public class ResourceService {
 
     private final ResourceRepository resourceRepository;
+    private final ResourceSearchRepository resourceSearchRepository;
 
-    public void createResource(List<ResourceRequestDto> resourceRequestDtoList, Board board){
+    public void createResource(List<ResourceRequestDto> resourceRequestDtoList, Board board) {
 
         resourceRequestDtoList.forEach(resourceRequestDto -> {
             Resource resource = Resource.builder()
@@ -25,16 +30,34 @@ public class ResourceService {
                     .board(board)
                     .build();
 
+            Optional<ResourceKeywordDocument> optionalResourceKeywordDocument = resourceSearchRepository.findByResourceName(resource.getResourceName());
+            if (optionalResourceKeywordDocument.isPresent()) {
+                optionalResourceKeywordDocument.get().plusCnt();
+            } else {
+                resourceSearchRepository.save(ResourceKeywordDocument.builder()
+                        .resource(resource)
+                        .build()
+                );
+            }
+
             resourceRepository.save(resource);
         });
     }
 
-    public void updateResource(List<ResourceRequestDto> resourceRequestDtoList, Board board){
+    public void updateResource(List<ResourceRequestDto> resourceRequestDtoList, Board board) {
         this.removeAllResource(board);
-        this.createResource(resourceRequestDtoList,board);
+        this.createResource(resourceRequestDtoList, board);
     }
 
     public void removeAllResource(Board board) {
+        List<String> resourceNameList = board.getResourceList().stream()
+                .map(Resource::getResourceName).collect(Collectors.toList());
+
+        resourceNameList.forEach(s ->
+                resourceSearchRepository.findByResourceName(s)
+                        .ifPresent(ResourceKeywordDocument::minusCnt)
+        );
+
         resourceRepository.deleteAll(resourceRepository.findAllByBoard(board));
     }
 }
