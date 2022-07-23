d09cdc38f1cb00ff4597abc258adf4e8c3c3324d
ojy9612 <pvnm@naver.com>
2022-07-23 11:36:12 +0900

    BoardService 단위 테스트 작성 #215

--- src/test/java/com/hanghae99_team3/model/board/BoardServiceTest.java
+++ src/test/java/com/hanghae99_team3/model/board/BoardServiceTest.java
@@ -7,10 +7,8 @@ import com.hanghae99_team3.model.board.repository.BoardRepository;
 import com.hanghae99_team3.model.board.service.BoardDocumentService;
 import com.hanghae99_team3.model.board.service.BoardService;
 import com.hanghae99_team3.model.images.ImagesService;
-import com.hanghae99_team3.model.recipestep.RecipeStepService;
 import com.hanghae99_team3.model.recipestep.dto.RecipeStepRequestDto;
 import com.hanghae99_team3.model.resource.dto.ResourceRequestDto;
-import com.hanghae99_team3.model.resource.service.ResourceService;
 import com.hanghae99_team3.model.user.UserService;
 import com.hanghae99_team3.model.user.domain.AuthProvider;
 import com.hanghae99_team3.model.user.domain.User;
@@ -23,6 +21,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,7 +31,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static com.hanghae99_team3.exception.ErrorMessage.BOARD_NOT_FOUND;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
@@ -40,6 +41,7 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 @DisplayName("BoardService 테스트")
 class BoardServiceTest {
+
 
     @Mock
     BoardRepository boardRepository;
@@ -49,10 +51,6 @@ class BoardServiceTest {
     UserService userService;
     @Mock
     ImagesService imagesService;
-    @Mock
-    ResourceService resourceService;
-    @Mock
-    RecipeStepService recipeStepService;
     @InjectMocks
     BoardService boardService;
 
@@ -61,7 +59,7 @@ class BoardServiceTest {
 
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         baseUser = User.testRegister()
                 .email("email@test.com")
                 .password("password")
@@ -78,7 +76,7 @@ class BoardServiceTest {
 
     @Nested
     @DisplayName("성공 테스트")
-    class SuccessTest{
+    class SuccessTest {
 
         @Test
         @DisplayName("ID로 게시글 조회")
@@ -88,16 +86,17 @@ class BoardServiceTest {
                     .user(baseUser)
                     .build();
 
-            //when
+            boardRepository.save(board);
+
+//            when
             when(boardRepository.findById(
                     anyLong()
             )).thenReturn(Optional.of(board));
 
-            Board resultBoard = boardService.findBoardById(1L);
+            Board resultBoard = boardService.findBoardById(board.getId());
 
             //then
             assertThat(resultBoard.getUser().getEmail()).isEqualTo("email@test.com");
-
         }
 
         @Test
@@ -105,7 +104,7 @@ class BoardServiceTest {
         void getBoardsBySortPreview() {
             //given
             List<Board> boardList = new ArrayList<>();
-            for(int i = 0; i < 10; i ++){
+            for (int i = 0; i < 10; i++) {
                 boardList.add(Board.emptyBuilder()
                         .user(baseUser)
                         .build()
@@ -121,7 +120,6 @@ class BoardServiceTest {
 
             //then
             assertThat(resultBoard.get(0).getUser().getEmail()).isEqualTo("email@test.com");
-
         }
 
         @Test
@@ -129,7 +127,7 @@ class BoardServiceTest {
         void getAllBoardsBySort() {
             //given
             List<Board> boardList = new ArrayList<>();
-            for(int i = 0; i < 5; i ++){
+            for (int i = 0; i < 5; i++) {
                 boardList.add(Board.emptyBuilder()
                         .user(baseUser)
                         .build()
@@ -137,7 +135,7 @@ class BoardServiceTest {
             }
 
             Page<Board> boardPage = new PageImpl<>(boardList);
-            Pageable pageable = PageRequest.of(0,1);
+            Pageable pageable = PageRequest.of(0, 1);
 
             //when
             when(boardRepository.findAll(
@@ -176,7 +174,6 @@ class BoardServiceTest {
 
             //then
             assertThat(resultImageLink).isEqualTo("imageLink");
-
         }
 
         @Test
@@ -204,8 +201,8 @@ class BoardServiceTest {
             Board resultBoard = boardService.checkModifyingBoard(baseUserDetails);
             //then
             assertThat(resultBoard).isEqualTo(board);
-
         }
+
         @Test
         @DisplayName("작성중인 게시글 확인 - 작성중인 게시글 있을 때")
         void checkModifyingBoard2() {
@@ -227,7 +224,6 @@ class BoardServiceTest {
             Board resultBoard = boardService.checkModifyingBoard(baseUserDetails);
             //then
             assertThat(resultBoard).isEqualTo(board);
-
         }
 
         @Test
@@ -236,7 +232,7 @@ class BoardServiceTest {
             //given
             List<ResourceRequestDto> resourceRequestDtoList = new ArrayList<>();
 
-            for(int i = 0; i < 2; i ++){
+            for (int i = 0; i < 2; i++) {
                 resourceRequestDtoList.add(ResourceRequestDto.builder()
                         .resourceName("재료 이름")
                         .amount("재료 수량")
@@ -276,12 +272,117 @@ class BoardServiceTest {
                     anyLong()
             )).thenReturn(Optional.of(board));
 
+            boardService.createTempBoard(baseUserDetails, 1L, boardRequestDto);
+
+            //then
+            assertThat(board.getTitle()).isEqualTo("제목");
+            assertThat(board.getContent()).isEqualTo("내용");
+            assertThat(board.getMainImageLink()).isEqualTo("이미지 Link");
+            assertThat(board.getStatus()).isEqualTo("modifying");
+        }
+
+        @Test
+        @DisplayName("게시글 생성")
+        void createBoard() {
+            //given
+            List<ResourceRequestDto> resourceRequestDtoList = new ArrayList<>();
+
+            for (int i = 0; i < 2; i++) {
+                resourceRequestDtoList.add(ResourceRequestDto.builder()
+                        .resourceName("재료 이름")
+                        .amount("재료 수량")
+                        .category("재료 카테고리")
+                        .build()
+                );
+            }
+
+            List<RecipeStepRequestDto> recipeStepRequestDtoList = new ArrayList<>();
+            for (int i = 0; i < 2; i++) {
+                recipeStepRequestDtoList.add(RecipeStepRequestDto.builder()
+                        .stepNum(i)
+                        .stepContent("레시피 Step 내용")
+                        .imageLink("레시피 이미지 Link")
+                        .build()
+                );
+            }
+
+            BoardRequestDto boardRequestDto = BoardRequestDto.builder()
+                    .title("제목")
+                    .content("내용")
+                    .mainImageLink("이미지 Link")
+                    .resourceRequestDtoList(resourceRequestDtoList)
+                    .recipeStepRequestDtoList(recipeStepRequestDtoList)
+                    .build();
+
+            Board board = Board.emptyBuilder()
+                    .user(baseUser)
+                    .build();
+
+            //when
+            when(userService.findUserByAuthEmail(
+                    any(PrincipalDetails.class)
+            )).thenReturn(baseUser);
 
             when(boardRepository.findById(
                     anyLong()
             )).thenReturn(Optional.of(board));
 
-            boardService.createTempBoard(baseUserDetails,1L,boardRequestDto);
+            boardService.createBoard(baseUserDetails, 1L, boardRequestDto);
+
+            //then
+            assertThat(board.getTitle()).isEqualTo("제목");
+            assertThat(board.getContent()).isEqualTo("내용");
+            assertThat(board.getMainImageLink()).isEqualTo("이미지 Link");
+            assertThat(board.getStatus()).isEqualTo("complete");
+        }
+
+        @Test
+        @DisplayName("게시글 수정")
+        void updateBoard() {
+            //given
+            List<ResourceRequestDto> resourceRequestDtoList = new ArrayList<>();
+
+            for (int i = 0; i < 2; i++) {
+                resourceRequestDtoList.add(ResourceRequestDto.builder()
+                        .resourceName("재료 이름")
+                        .amount("재료 수량")
+                        .category("재료 카테고리")
+                        .build()
+                );
+            }
+
+            List<RecipeStepRequestDto> recipeStepRequestDtoList = new ArrayList<>();
+            for (int i = 0; i < 2; i++) {
+                recipeStepRequestDtoList.add(RecipeStepRequestDto.builder()
+                        .stepNum(i)
+                        .stepContent("레시피 Step 내용")
+                        .imageLink("레시피 이미지 Link")
+                        .build()
+                );
+            }
+
+            BoardRequestDto boardRequestDto = BoardRequestDto.builder()
+                    .title("제목")
+                    .content("내용")
+                    .mainImageLink("이미지 Link")
+                    .resourceRequestDtoList(resourceRequestDtoList)
+                    .recipeStepRequestDtoList(recipeStepRequestDtoList)
+                    .build();
+
+            Board board = Board.emptyBuilder()
+                    .user(baseUser)
+                    .build();
+
+            //when
+            when(userService.findUserByAuthEmail(
+                    any(PrincipalDetails.class)
+            )).thenReturn(baseUser);
+
+            when(boardRepository.findById(
+                    anyLong()
+            )).thenReturn(Optional.of(board));
+
+            boardService.updateBoard(baseUserDetails, 1L, boardRequestDto);
 
             //then
             assertThat(board.getTitle()).isEqualTo("제목");
@@ -290,14 +391,103 @@ class BoardServiceTest {
         }
 
         @Test
-        @DisplayName("게시글 생성")
-        void createBoard() {
+        @DisplayName("게시글 삭제")
+        void deleteBoard(){
+            //given
+            List<ResourceRequestDto> resourceRequestDtoList = new ArrayList<>();
+            for (int i = 0; i < 2; i++) {
+                resourceRequestDtoList.add(ResourceRequestDto.builder()
+                        .resourceName("재료 이름")
+                        .amount("재료 수량")
+                        .category("재료 카테고리")
+                        .build()
+                );
+            }
 
+            List<RecipeStepRequestDto> recipeStepRequestDtoList = new ArrayList<>();
+            for (int i = 0; i < 2; i++) {
+                recipeStepRequestDtoList.add(RecipeStepRequestDto.builder()
+                        .stepNum(i)
+                        .stepContent("레시피 Step 내용")
+                        .imageLink("레시피 이미지 Link")
+                        .build()
+                );
+            }
+
+            BoardRequestDto boardRequestDto = BoardRequestDto.builder()
+                    .title("제목")
+                    .content("내용")
+                    .mainImageLink("이미지 Link")
+                    .resourceRequestDtoList(resourceRequestDtoList)
+                    .recipeStepRequestDtoList(recipeStepRequestDtoList)
+                    .build();
+
+            Board board = Board.builder()
+                    .user(baseUser)
+                    .boardRequestDto(boardRequestDto)
+                    .build();
+
+            //when
+            when(userService.findUserByAuthEmail(
+                    any(PrincipalDetails.class)
+            )).thenReturn(baseUser);
+
+            when(boardRepository.findById(
+                    anyLong()
+            )).thenReturn(Optional.of(board));
+
+            boardService.deleteBoard(baseUserDetails, 1L);
+
+            //then
+            //?? 어캐 검증해야 하죠?
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("실패 테스트")
+    class FailTest{
+
+        @Test
+        @DisplayName("게시글 하나 불러오기 - 존재하지 않는 게시글 접근")
+        void findBoardByIdWrongId(){
+            //given
+            //when
+            when(boardRepository.findById(
+                    anyLong()
+            )).thenThrow(new IllegalArgumentException(BOARD_NOT_FOUND));
+
+            Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                    boardService.findBoardById(1L)
+            );
+            //then
+            assertThat(exception.getMessage()).isEqualTo(BOARD_NOT_FOUND);
         }
 
         @Test
-        void deleteBoard() {
+        @DisplayName("게시글 10개만 미리 보기 - 잘못된 엔티티 이름 주입")
+        void getBoardsBySortPreviewWrongEntityName() {
+            //given
+            List<Board> boardList = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                boardList.add(Board.emptyBuilder()
+                        .user(baseUser)
+                        .build()
+                );
+            }
+
+            //when
+//            when(boardRepository.findFirst10By(
+//                    any(Sort.class)
+//            )).thenThrow(new PropertyReferenceException());
+
+            List<Board> resultBoard = boardService.getBoardsBySortPreview("goodCounta");
+
+            //then
+            assertThat(resultBoard.get(0).getUser().getEmail()).isEqualTo("email@test.com");
         }
+
     }
 
 }
