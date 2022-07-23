53f95bfb723303727860911e34d9d2078b86f4ee
ojy9612 <pvnm@naver.com>
2022-07-23 11:35:02 +0900

    comment 예외처리 추가 #18

--- src/main/java/com/hanghae99_team3/exception/newException/IdDuplicateException.java
+++
@@ -1,8 +0,0 @@
-package com.hanghae99_team3.exception.newException;
-
-public class IdDuplicateException extends RuntimeException{
-
-    public IdDuplicateException(String message) {
-        super(message);
-    }
-}
