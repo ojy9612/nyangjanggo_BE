db7bd8b1a6191b9d26bae99a6b178c7a7b27a5ae
ojy9612 <pvnm@naver.com>
2022-07-26 21:11:37 +0900

    서버 port번호 테스트

--- src/main/java/com/hanghae99_team3/model/board/testController.java
+++ src/main/java/com/hanghae99_team3/model/board/testController.java
@@ -16,6 +16,11 @@ import com.hanghae99_team3.model.resource.service.ResourceService;
 import com.hanghae99_team3.model.user.UserService;
 import com.hanghae99_team3.model.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.elasticsearch.common.inject.Inject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -25,6 +30,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,6 +49,7 @@ public class testController {
     private final UserService userService;
     private final ResourceService resourceService;
     private final RecipeStepService recipeStepService;
+    private final ServletWebServerApplicationContext webServerAppCtxt;
 
 
     @GetMapping("/api/boards/elastic")
@@ -96,6 +106,25 @@ public class testController {
             boardRepository.save(board);
         });
 
+    }
+
+    @GetMapping("/test/port")
+    public Integer checkPort() throws IOException {
+
+        Integer port = webServerAppCtxt.getWebServer().getPort();
+
+        BufferedReader reader = new BufferedReader(
+                new FileReader("/home/ec2-user/service_url.inc")
+        );
+
+        String str = reader.readLine();
+        str = str.substring(str.length()-5).substring(0,4);
+
+        Integer processingPort = Integer.valueOf(str);
+
+        System.out.println(processingPort);
+
+        return port;
     }
 
 }
